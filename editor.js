0bedb9a70d7e70e7f002fbe95cb3afa5c513829e
puyon7676 <ICD30829@nifty.com>
2025-11-24 10:44:16 +0900

    Add files via upload

--- editor.js
+++ editor.js
@@ -11,6 +11,30 @@
 	// グループエディタ用のカラム定義
 	function getGroupColumns()
 	{
+		return [
+			{title:"名前", field:"name", editor:"input"},
+			{title:"有利グループ", field:"AdvantageousAttr", editor:"list",
+				editorParams:{
+				  values:values
+				},
+				formatter:function(cell){
+					const value = cell.getValue();
+					return mapA[value] || value;
+				}
+			},
+			{title:"不利グループ", field:"DisadvantageousAttr", editor:"list",
+				editorParams:{
+				  values:values
+				},
+				formatter:function(cell){
+					const value = cell.getValue();
+					return mapD[value] || value;
+				}
+			},
+			{title:"攻略サイトURL", field:"siteURL", editor:"input"}
+		];
+	}
+
 		values = [
 			{label:"グループ01", value:"Type01"},
 			{label:"グループ02", value:"Type02"},
@@ -23,7 +47,19 @@
 			{label:"グループ09", value:"Type09"},
 			{label:"グループ10", value:"Type10"},
 		];
-		const map = {
+		const mapA = {
+			"Type01":"グループ01",
+			"Type02":"グループ02",
+			"Type03":"グループ03",
+			"Type04":"グループ04",
+			"Type05":"グループ05",
+			"Type06":"グループ06",
+			"Type07":"グループ07",
+			"Type08":"グループ08",
+			"Type09":"グループ09",
+			"Type10":"グループ10",
+		};
+		const mapD = {
 			"Type01":"グループ01",
 			"Type02":"グループ02",
 			"Type03":"グループ03",
@@ -36,28 +72,25 @@
 			"Type10":"グループ10",
 		};
 
-		return [
-			{title:"名前", field:"name", editor:"input"},
-			{title:"有利グループ", field:"AdvantageousAttr", editor:"list",
-				editorParams:{
-				  values:values
-				},
-				formatter:function(cell){
-					const value = cell.getValue();
-					return map[value] || value;
-				}
-			},
-			{title:"不利グループ", field:"DisadvantageousAttr", editor:"list",
-				editorParams:{
-				  values:values
-				},
-				formatter:function(cell){
-					const value = cell.getValue();
-					return map[value] || value;
-				}
-			},
-			{title:"攻略サイトURL", field:"siteURL", editor:"input"}
-		];
-	}
+var table = createEditor("#group-editor1", getGroupColumns(), "data.json");
 
-var table1 = createEditor("#group-editor1", getGroupColumns(), "data.json");
+table.on("cellEdited", function(cell){
+	if(cell.getField() === "name"){
+		let row = cell.getRow();
+		let data = row.getData();
+		let typeKey = "Type" + String(data.id).padStart(2,"0");
+
+		mapA[typeKey] = data.name;
+		values[data.id - 1].label = data.name;
+
+		// 内部値を更新したい場合
+		row.update({group:typeKey});
+
+		// 全行を取得
+		let rows = table.getRows();
+		// 1行ずつ再描画
+		rows.forEach(row => {
+			row.reformat();
+		});
+	}
+});
